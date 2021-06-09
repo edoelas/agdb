ae85bdf3db0e6df383257a148b6cb4d1fff70daf
edoelas <miego.cvcv@gmail.com>
2021-06-09 13:22:23 +0200

    test 2

--- _plugins/test.py
+++ _plugins/test.py
@@ -5,6 +5,8 @@ md_file = open("./_plugins/temp.md", "r")
 md_string = md_file.read()
 md_file.close()
 
+md_string = "TEST" + md_string
+
 html_string = markdown.markdown(md_string, extensions=["tables"])
 
 html_file = open("./_plugins/temp.html", "w")
